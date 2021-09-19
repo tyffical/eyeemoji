1f8b3a3376b6299953142987db998d374e4d9c16
Tiffany Trinh <34641130+tyffical@users.noreply.github.com>
2021-09-19 14:32:14 +0000

    feat: added user input and completion output in terminal

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ import openai
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.environ['OPENAI_API_KEY']
 
-response = openai.Completion.create(engine="davinci", prompt="This is a test", max_tokens=5)
+print("Type whatever you want, then press Enter! :)")
+prompt = input()
+
+test_prompt = "This is a test"
+
+response = openai.Completion.create(engine="davinci", prompt=prompt, max_tokens=5, echo=True)
+
+print(response["choices"][0]["text"])
